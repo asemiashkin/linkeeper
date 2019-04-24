e4ef3682c68ee73287c2902bdfaac53fe3a0c949
Artem Semiashkin <mmf51@protonmail.com>
2019-04-24 20:57:14 +0300

    create, update

--- src/persistence/dao/UsersDaoRepository.kt
+++ src/persistence/dao/UsersDaoRepository.kt
@@ -1,16 +1,27 @@
 package com.asemiashkin.persistence.dao
 
 import com.asemiashkin.User
+import com.asemiashkin.Users
 import com.asemiashkin.persistence.UsersRepository
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
 
 class UsersDaoRepository : UsersRepository {
     override fun create(toAdd: User): Long = transaction {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        (Users.insert {
+            it[nickname] = toAdd.nickname
+            it[email] = toAdd.email
+            it[role] = toAdd.role.toString()
+        } get Users.id)!!
     }
 
     override fun update(toUpdate: User): Boolean = transaction {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Users.update({Users.id eq toUpdate.id}) {
+            it[nickname] = toUpdate.nickname
+            it[email] = toUpdate.email
+            it[role] = toUpdate.role.toString()
+        } > 0
     }
 
     override fun remove(id: Long): Boolean = transaction {
